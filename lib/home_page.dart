b05f51b5355e0a5ece0c32b3a58e179397517531
Khalil's MAC <khalilobedjaoui@gmail.com>
2023-10-10 10:54:12 +0200

    Barre de navigation

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -8,10 +8,24 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   double? soldeInitial = 1000.0;
+  int _selectedIndex = 0;
   List<Stock> stocks = [
     Stock(name: "Tesla", category: "Automobile", quantity: 5),
     Stock(name: "McDonald's", category: "Restauration", quantity: 3),
   ];
+
+  static List<Widget> _widgetOptions = <Widget>[
+    Text('Page d\'accueil'),
+    Text('Cours de la bourse'),
+    Text('Profil'),
+    Text('Réglages'),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,32 +46,37 @@ class _HomePageState extends State<HomePage> {
             ),
             SizedBox(height: 20),
             Expanded(
-              child: ListView.builder(
-                itemCount: stocks.length,
-                itemBuilder: (context, index) {
-                  final stock = stocks[index];
-                  return ListTile(
-                    title: Text(stock.name),
-                    subtitle: Text(stock.category),
-                    trailing: Text('${stock.quantity}'),
-                  );
-                },
-              ),
-            ),
-            ElevatedButton(
-              onPressed: _buyStock,
-              child: Text("Acheter des actions"),
-            ),
-            ElevatedButton(
-              onPressed: _sellStock,
-              child: Text("Vendre des actions"),
+              child: _widgetOptions.elementAt(_selectedIndex),
             ),
           ],
         ),
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Accueil',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.show_chart),
+            label: 'Bourse',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profil',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Réglages',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        unselectedItemColor: Colors.grey,
+        onTap: _onItemTapped,
+      ),
     );
   }
-
   void _buyStock() {
     // Logique d'achat d'actions
   }
@@ -66,3 +85,6 @@ class _HomePageState extends State<HomePage> {
     // Logique de vente d'actions
   }
 }
+
+
+
