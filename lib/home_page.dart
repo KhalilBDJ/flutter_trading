cfd3a85c69a8782218adb096243a5d2bd760d9a8
Khalil's MAC <khalilobedjaoui@gmail.com>
2023-10-10 10:46:46 +0200

    home_page change

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:fl_chart/fl_chart.dart';
-import 'package:flutter_credit_card/credit_card_brand.dart';
-import 'package:flutter_credit_card/flutter_credit_card.dart';
 import 'classes/stock.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,139 +7,62 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  double? soldeInitial;
-  String firstName = "Magrows";
-  String lastName = "Beat";
-  int touchedIndex = -1;
+  double? soldeInitial = 1000.0;
   List<Stock> stocks = [
     Stock(name: "Tesla", category: "Automobile", quantity: 5),
     Stock(name: "McDonald's", category: "Restauration", quantity: 3),
-    // Ajoutez d'autres stocks ici
   ];
 
   @override
   Widget build(BuildContext context) {
-    Map<String, int> categoriesCount = _getCategoriesCount();
-
     return Scaffold(
       appBar: AppBar(
-        title: Text(' Portefeuille'),
+        title: Text('Gestion de Portefeuille'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
-            CreditCardWidget(
-              cardNumber: "9485 4848 4938 8888",
-              expiryDate: "12/25",
-              cardHolderName: '$firstName $lastName',
-              cvvCode: "123",
-              showBackView: false, onCreditCardWidgetChange: (CreditCardBrand creditCardBrand) {},
-            ),
-            SizedBox(height: 20),
-            Text('Solde actuel: $soldeInitial'),
-            SizedBox(height: 20),
-            Expanded(
-              child: PieChart(
-                PieChartData(
-                  sections: _buildPieChartSections(categoriesCount),
-                  borderData: FlBorderData(show: false),
-                  sectionsSpace: 0,
-                  centerSpaceRadius: 40,
-                  pieTouchData: PieTouchData(
-                    touchCallback: (pieTouchResponse) {
-                      setState(() {
-                        if (pieTouchResponse.touchInput is FlLongPressEndTouchInput ||
-                            pieTouchResponse.touchInput is FlPanEndTouchInput) {
-                          touchedIndex = -1;
-                        } else if (pieTouchResponse.touchInput is FlTouchStart ||
-                            pieTouchResponse.touchInput is FlTouchMove) {
-                          touchedIndex = pieTouchResponse.touchedSection!.touchedSectionIndex;
-                        }
-                      });
-                    },
-                  ),
-                ),
-                ),
+            Text(
+              'Solde actuel: \$${soldeInitial?.toStringAsFixed(2)}',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
               ),
             ),
-            ..._buildLegend(categoriesCount),
+            SizedBox(height: 20),
+            Expanded(
+              child: ListView.builder(
+                itemCount: stocks.length,
+                itemBuilder: (context, index) {
+                  final stock = stocks[index];
+                  return ListTile(
+                    title: Text(stock.name),
+                    subtitle: Text(stock.category),
+                    trailing: Text('${stock.quantity}'),
+                  );
+                },
+              ),
+            ),
+            ElevatedButton(
+              onPressed: _buyStock,
+              child: Text("Acheter des actions"),
+            ),
+            ElevatedButton(
+              onPressed: _sellStock,
+              child: Text("Vendre des actions"),
+            ),
           ],
         ),
       ),
     );
   }
 
-  Map<String, int> _getCategoriesCount() {
-    Map<String, int> categoriesCount = {};
-
-    for (var stock in stocks) {
-      if (categoriesCount.containsKey(stock.category)) {
-        categoriesCount[stock.category] = categoriesCount[stock.category]! + stock.quantity;
-      } else {
-        categoriesCount[stock.category] = stock.quantity;
-      }
-    }
-
-    return categoriesCount;
+  void _buyStock() {
+    // Logique d'achat d'actions
   }
 
-  List<Widget> _buildLegend(Map<String, int> categoriesCount) {
-    return categoriesCount.keys.map((category) {
-      final color = _getCategoryColor(category);
-      return Padding(
-        padding: const EdgeInsets.symmetric(vertical: 4.0),
-        child: Row(
-          children: [
-            Container(
-              width: 12,
-              height: 12,
-              color: color,
-            ),
-            SizedBox(width: 8),
-            Text(category),
-          ],
-        ),
-      );
-    }).toList();
-  }
-
-  List<PieChartSectionData> _buildPieChartSections(Map<String, int> categoriesCount) {
-    List<PieChartSectionData> sections = [];
-    int index = 0;
-
-    categoriesCount.forEach((category, count) {
-      final isTouched = index == touchedIndex;
-      final double fontSize = isTouched ? 25 : 16;
-      final double radius = isTouched ? 60 : 50;
-      sections.add(
-        PieChartSectionData(
-          color: _getCategoryColor(category),
-          value: count.toDouble(),
-          title: '',
-          radius: radius,
-          titleStyle: TextStyle(
-            fontSize: fontSize,
-            fontWeight: FontWeight.bold,
-            color: const Color(0xffffffff),
-          ),
-        ),
-      );
-      index++;
-    });
-
-    return sections;
-  }
-
-  Color _getCategoryColor(String category) {
-    switch (category) {
-      case 'Automobile':
-        return Colors.red;
-      case 'Restauration':
-        return Colors.blue;
-    // Ajoutez d'autres catégories et couleurs ici
-      default:
-        return Colors.grey;
-    }
+  void _sellStock() {
+    // Logique de vente d'actions
   }
 }
